6ccf6690090008feb2c4ea785f11bf76e905d3e9
Mustafizur Rahman <mustafizur232@gmail.com>
2024-01-09 19:52:54 +0600

    floder structure created for the project

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+export default router;
