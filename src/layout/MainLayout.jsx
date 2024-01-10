f62a5abd72b49a50e9813811f9585db79a95bdfe
Mustafizur Rahman <mustafizur232@gmail.com>
2024-01-10 13:04:41 +0600

    marquee header and navbar implemented and home layout redesigned

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../pages/Shared/Header";
 import Navbar from "../pages/Shared/Navbar";
-
+import BreakingNews from "../pages/Home/BreakingNews";
 const MainLayout = () => {
     return (
         <div className="max-w-6xl mx-auto font-poppins">
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -2,8 +2,8 @@ import logo from '../../assets/logo.png'
 import moment from 'moment';
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='text-center'>
+            <img className='mx-auto' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
             <p className='text-2xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
