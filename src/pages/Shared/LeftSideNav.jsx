2b6d39de73147d8727f382317ae98cb36fb1230b
Mustafizur Rahman <mustafizur232@gmail.com>
2024-01-10 15:50:12 +0600

    categories fake data loaded by using fetch and displayed on the leftside navigation of UI

--- src/pages/Shared/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('/categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
-        <div>
-            <div className="text-2xl">Left Side!!!</div>
+        <div className="px-4 space-y-6">
+            <h2 className="text-2xl">All Categories: {categories.length}</h2>
+            {
+                categories?.map(category => <Link to={`/category/${category.id}`} className="block text-xl font-semibold font-poppins" key={category.id}>{category.name}</Link>
+                )
+            }
         </div>
     );
 };
